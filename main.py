ef256da224cb3992db38f9020a6de22283199eb6
Max <maksym.solomyanov@gmail.com>
2024-05-15 21:29:52 +0200

    Create camera screen, start and stop the camera, capture photo.

---
+++ main.py
@@ -0,0 +1,44 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+from filesharer import FileSharer
+import time
+import os
+
+Builder.load_file('frontend.kv')
+
+
+class RootWidget(ScreenManager):
+    pass
+
+
+class MainApp(App):
+    def build(self):
+        return RootWidget()
+
+
+class CameraScreen(Screen):
+    def start(self):
+        self.ids.camera.play = True
+        self.ids.camera_button.text = "Stop Camera"
+        self.ids.camera.texture = self.ids.camera._camera.texture
+
+
+    def stop(self):
+        self.ids.camera.play = False
+        self.ids.camera_button.text = "Start Camera"
+        self.ids.camera.texture = None
+
+    def capture(self):
+        if not os.path.exists("files"):
+            os.makedirs("files")
+        current_time = time.strftime('%Y%m%d-%H%M%S')
+        filepath = f"files/{current_time}.png"
+        self.ids.camera.export_to_png(filepath)
+
+
+class ImageScreen(Screen):
+    pass
+
+
+MainApp().run()
